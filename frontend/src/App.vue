00dd04c9b4836d81ac01ce0202c5eb0a513eda28
dreamhunter2333 <dreamhunter2333@gmail.com>
2023-04-10 00:46:30 +0800

    init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -32,6 +32,7 @@ const onSubmit = async () => {
 
 onMounted(() => {
   (window.adsbygoogle = window.adsbygoogle || []).push({});
+  (window.adsbygoogle = window.adsbygoogle || []).push({});
 });
 </script>
 
