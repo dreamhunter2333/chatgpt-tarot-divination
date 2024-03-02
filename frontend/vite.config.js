21b72a10a0b02a4b249866c7195d20031c7a2a3f
dreamhunter2333 <dreamhunter2333@gmail.com>
2024-03-02 14:40:38 +0800

    feat: support PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,41 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // build: {
   //   outDir: '../dist',
   // },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'AI 占卜',
+        short_name: 'AI 占卜',
+        description: 'AI 占卜',
+        icons: [
+          {
+            "src": "/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+        theme_color: "#FFFFFF",
+        background_color: "#FFFFFF",
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
