648c357c9d31c4e08c74dd39bfbbfab2da080a88
dreamhunter2333 <dreamhunter2333@gmail.com>
2024-03-05 12:52:03 +0800

    feat: update logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { NGrid, NGi, NSpace, NAlert, NButton, NMessageProvider } from 'naive-ui'
+import { NConfigProvider, NGlobalStyle, NBackTop } from 'naive-ui';
 import { onMounted, ref, computed } from "vue";
 import { useRouter } from 'vue-router'
 import { useStorage } from '@vueuse/core'
 import { useIsMobile } from './utils/composables'
+import { zhCN } from 'naive-ui'
 
 const state_jwt = useStorage('jwt')
 const isMobile = useIsMobile()
@@ -39,46 +41,50 @@ onMounted(async () => {
 </script>
 
 <template>
-  <n-message-provider>
-    <n-grid x-gap="12" :cols="isMobile ? 4 : 6">
-      <n-gi v-if="!isMobile">
-        <div class="side">
-          <ins class="adsbygoogle" style="display:block" :data-ad-client="settings.ad_client"
-            :data-ad-slot="settings.ad_slot" data-ad-format="auto" data-full-width-responsive="true"></ins>
-        </div>
-      </n-gi>
-      <n-gi span="4">
-        <div class="main">
-          <n-space vertical>
-            <h2>AI 占卜 - 本项目仅供娱乐</h2>
-            <n-layout>
-              <n-alert v-if="settings.user_name" type="success">
-                你好, {{ settings.login_type }} 用户 {{ settings.user_name }}
-                <n-button tertiary type="primary" round @click="logOut">登出</n-button>
-                <n-button tag="a" target="_blank" tertiary type="primary" round
-                  href="https://github.com/dreamhunter2333/chatgpt-tarot-divination">☆ Github</n-button>
-              </n-alert>
-              <n-alert v-else type="warning">
-                当前未登录, 处于限流模式 ({{ settings.rate_limit }})
-                <n-button type="warning" round @click="router.push('/login')">登录</n-button>
-                <n-button tag="a" target="_blank" tertiary type="primary" round
-                  href="https://github.com/dreamhunter2333/chatgpt-tarot-divination">☆ Github</n-button>
-              </n-alert>
+  <n-config-provider :locale="zhCN">
+    <n-global-style />
+    <n-message-provider>
+      <n-grid x-gap="12" :cols="isMobile ? 4 : 6">
+        <n-gi v-if="!isMobile">
+          <div class="side">
+            <ins class="adsbygoogle" style="display:block" :data-ad-client="settings.ad_client"
+              :data-ad-slot="settings.ad_slot" data-ad-format="auto" data-full-width-responsive="true"></ins>
+          </div>
+        </n-gi>
+        <n-gi span="4">
+          <div class="main">
+            <n-space vertical>
+              <h2>AI 占卜 - 本项目仅供娱乐</h2>
               <n-layout>
-                <router-view></router-view>
+                <n-alert v-if="settings.user_name" type="success">
+                  你好, {{ settings.login_type }} 用户 {{ settings.user_name }}
+                  <n-button tertiary type="primary" round @click="logOut">登出</n-button>
+                  <n-button tag="a" target="_blank" tertiary type="primary" round
+                    href="https://github.com/dreamhunter2333/chatgpt-tarot-divination">☆ Github</n-button>
+                </n-alert>
+                <n-alert v-else type="warning">
+                  当前未登录, 处于限流模式 ({{ settings.rate_limit }})
+                  <n-button type="warning" round @click="router.push('/login')">登录</n-button>
+                  <n-button tag="a" target="_blank" tertiary type="primary" round
+                    href="https://github.com/dreamhunter2333/chatgpt-tarot-divination">☆ Github</n-button>
+                </n-alert>
+                <n-layout>
+                  <router-view></router-view>
+                </n-layout>
               </n-layout>
-            </n-layout>
-          </n-space>
-        </div>
-      </n-gi>
-      <n-gi v-if="!isMobile">
-        <div class="side">
-          <ins class="adsbygoogle" style="display:block" :data-ad-client="settings.ad_client"
-            :data-ad-slot="settings.ad_slot" data-ad-format="auto" data-full-width-responsive="true"></ins>
-        </div>
-      </n-gi>
-    </n-grid>
-  </n-message-provider>
+            </n-space>
+          </div>
+        </n-gi>
+        <n-gi v-if="!isMobile">
+          <div class="side">
+            <ins class="adsbygoogle" style="display:block" :data-ad-client="settings.ad_client"
+              :data-ad-slot="settings.ad_slot" data-ad-format="auto" data-full-width-responsive="true"></ins>
+          </div>
+        </n-gi>
+      </n-grid>
+    </n-message-provider>
+    <n-back-top />
+  </n-config-provider>
 </template>
 
 <style scoped>
