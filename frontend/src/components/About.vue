6827c06b45a7374b73fe2a7de353947d017ae953
Dream Hunter <dreamhunter2333@gmail.com>
2024-04-25 01:34:41 +0800

    feat: refactor ui (#56)

---
+++ frontend/src/components/About.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { NCard } from 'naive-ui';
+import MarkdownIt from 'markdown-it';
+import { ABOUT } from "../config/constants";
+
+const md = new MarkdownIt();
+const about = md.render(ABOUT);
+</script>
+
+<template>
+    <n-card style="text-align: left;">
+        <div v-html="about"></div>
+    </n-card>
+</template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { registerSW } from 'virtual:pwa-register'
 
+registerSW({ immediate: true })
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
